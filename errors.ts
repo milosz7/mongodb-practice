c3f23f7042ff682876cb833065e41d34c47b887e
Milosz B <miloszbargiel@gmail.com>
2022-08-22 15:40:19 +0200

    Add error objects data

---
+++ errors.ts
@@ -0,0 +1,14 @@
+export const error404 = {
+  status: 404,
+  message: 'Not found.',
+};
+
+export const error400 = {
+  status: 400,
+  message: 'Bad request.',
+};
+
+export const error500 = {
+  status: 500,
+  message: 'Internal server error.',
+};
