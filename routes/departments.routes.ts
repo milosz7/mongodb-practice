4e6fb96d165fad6394b10fda7e16ae1c54490b60
Milosz B <miloszbargiel@gmail.com>
2022-08-22 15:46:07 +0200

    Departments routes code reformatting

--- routes/departments.routes.ts
+++ routes/departments.routes.ts
@@ -10,16 +10,19 @@ router.get('/departments', (req, res, next) => {
     .collection('departments')
     .find()
     .toArray((err, data) => {
-      if (err) return next(error500)
+      if (err) return next(error500);
       return res.json(data);
-  })
+    });
 });
 
 router.get('/departments/random', (req, res, next) => {
-  req.db.collection<departmentData>('departments').aggregate([{ $sample: {size: 1}}]).toArray((err, data) => {
-    if (err) next(error500);
-    return res.json(...data!);
-  })
+  req.db
+    .collection<departmentData>('departments')
+    .aggregate([{ $sample: { size: 1 } }])
+    .toArray((err, data) => {
+      if (err) next(error500);
+      return res.json(...data!);
+    });
 });
 
 router.get('/departments/:id', (req, res, next) => {
@@ -37,41 +40,41 @@ router.post('/departments', (req, res, next) => {
     req.db.collection('departments').insertOne({ name }, (err) => {
       if (err) return next(error500);
       return res.status(200).send('OK');
-    })
+    });
   }
-  if (!name) next(error400)
+  if (!name) next(error400);
 });
 
 router.put('/departments/:id', (req, res, next) => {
-  const { name }: {name: string | undefined} = req.body;
+  const { name }: { name: string | undefined } = req.body;
   if (!name) {
-    return next(error400)
+    return next(error400);
   }
   const query = { _id: new ObjectId(req.params.id) };
   const dataToEdit = req.db.collection('departments').findOne(query, (err, data) => {
-    if (err) return next(error500)
+    if (err) return next(error500);
     if (!data) return next(error404);
     if (data) {
-      req.db.collection('departments').updateOne(query, {$set: {name: name}}, (err) => {
-        if (err) next(error500)
+      req.db.collection('departments').updateOne(query, { $set: { name: name } }, (err) => {
+        if (err) next(error500);
       });
-      return res.status(200).send('OK');    
+      return res.status(200).send('OK');
     }
-  })
+  });
 });
 
 router.delete('/departments/:id', (req, res, next) => {
   const query = { _id: new ObjectId(req.params.id) };
   const dataToEdit = req.db.collection('departments').findOne(query, (err, data) => {
-    if(err) return next(error500)
+    if (err) return next(error500);
     if (!data) return next(error404);
     if (data) {
       req.db.collection('departments').deleteOne(query, (err) => {
-        if(err) return next(error500)
+        if (err) return next(error500);
       });
-      return res.status(200).send('OK');    
+      return res.status(200).send('OK');
     }
-  })
+  });
 });
 
 export default router;
