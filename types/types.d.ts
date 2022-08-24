d95a77eb8f70ba880e0bcff4d5e46b6ae7d14d13
Milosz B <miloszbargiel@gmail.com>
2022-08-24 21:06:02 +0200

    Remove unnecessary type definitions

--- types/types.d.ts
+++ types/types.d.ts
@@ -1,32 +1,4 @@
-import  { Db, ObjectId } from 'mongodb';
-
-declare global {
-  namespace Express {
-    interface Request {
-      db: Db
-    }
-  }
-}
-
-interface errorData {
+interface ErrorData {
   status: number
   message: string
-}
-
-interface employeeData {
-  _id?: ObjectId
-  firstName: string
-  lastName: string
-  department: string
-} 
-
-interface departmentData {
-  _id?: ObjectId
-  name: string;
-}
-
-interface productData {
-  _id?: ObjectId
-  name: string;
-  client: string;
 }
